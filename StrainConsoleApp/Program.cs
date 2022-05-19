73795d3c4e4ee6e31418d62ece4171fe1911012d
Tyler Stambaugh <tyler.stambaugh@gmail.com>
2022-05-18 20:41:39 -0400

    Add project files.

---
+++ StrainConsoleApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrainConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var keepReturn = Strain.Keep(new HashSet<int> { 1, 2, 3, 4, 5, 11, 12, 8 }, (x => x < 10));
+            foreach (var x in keepReturn)
+            {
+                global::System.Console.WriteLine(x);
+            }
+            Console.ReadKey();
+
+        }                
+    }
+
+    public static class Strain
+    {
+
+        public static IEnumerable<T> Keep<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            var returnList = new List<T>();
+
+            foreach (var item in collection)
+            {
+                if (predicate(item))
+                {
+                    returnList.Add(item);
+                }
+            }
+
+            return returnList;
+        }
+
+        public static IEnumerable<T> Discard<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            var returnList = new List<T>();
+
+            foreach (var item in collection)
+            {
+                if (!predicate(item))
+                {
+                    returnList.Add(item);
+                }
+            }
+
+            return returnList;
+        }
+    }
+}
